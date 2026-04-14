393ca534d1256607e5022fbe2417ce2386aa29e0
JJSinghRathore <jatinjaisinghrathore@gmail.com>
2026-04-14 10:09:36 +0530

    Updated

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,59 +1,8 @@
 const Task = require('../models/Task');
-const { validationResult } = require('express-validator');
-
-exports.createTask = async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) return res.status(400).json(errors.array());
-
-    const task = await Task.create({
-        ...req.body,
-        userId: req.user.id
-    });
-
-    res.json(task);
-};
-
-exports.getTasks = async (req, res) => {
-    const tasks = await Task.find({ userId: req.user.id });
-    res.json(tasks);
-};
-
-exports.getTask = async (req, res) => {
-    const task = await Task.findOne({
-        _id: req.params.id,
-        userId: req.user.id
-    });
-
-    if (!task) return res.status(404).json({ msg: "Not found" });
-
-    res.json(task);
-};
-
-exports.updateTask = async (req, res) => {
-    const task = await Task.findOneAndUpdate(
-        { _id: req.params.id, userId: req.user.id },
-        req.body,
-        { new: true }
-    );
-
-    if (!task) return res.status(404).json({ msg: "Not found" });
-
-    res.json(task);
-};
-
-exports.deleteTask = async (req, res) => {
-    const task = await Task.findOneAndDelete({
-        _id: req.params.id,
-        userId: req.user.id
-    });
-
-    if (!task) return res.status(404).json({ msg: "Not found" });
-
-    res.json({ msg: "Deleted" });
-};
-
+const axios = require('axios');
 const { scheduleReminder } = require('../utils/reminder');
 
+// CREATE TASK
 exports.createTask = async (req, res) => {
     const task = await Task.create({
         ...req.body,
@@ -63,4 +12,83 @@ exports.createTask = async (req, res) => {
     scheduleReminder(task);
 
     res.json(task);
+};
+
+// GET ALL TASKS
+exports.getTasks = async (req, res) => {
+    const tasks = await Task.find({ userId: req.user.id });
+    res.json(tasks);
+};
+
+// GET SINGLE TASK
+exports.getTaskById = async (req, res) => {
+    const task = await Task.findOne({
+        _id: req.params.id,
+        userId: req.user.id
+    });
+
+    if (!task) return res.status(404).json({ message: "Task not found" });
+
+    res.json(task);
+};
+
+// WEBHOOK FUNCTION (with retry)
+const sendWebhook = async (task, retries = 3) => {
+    try {
+        await axios.post(process.env.WEBHOOK_URL, {
+            id: task._id,
+            title: task.title,
+            userId: task.userId,
+            completedAt: new Date()
+        });
+    } catch (err) {
+        if (retries > 0) {
+            setTimeout(() => sendWebhook(task, retries - 1), 2000);
+        }
+    }
+};
+
+// UPDATE TASK
+exports.updateTask = async (req, res) => {
+    const task = await Task.findOneAndUpdate(
+        { _id: req.params.id, userId: req.user.id },
+        req.body,
+        { new: true }
+    );
+
+    if (!task) return res.status(404).json({ message: "Task not found" });
+
+    scheduleReminder(task);
+
+    if (req.body.status === 'completed') {
+        sendWebhook(task);
+    }
+
+    res.json(task);
+};
+
+// DELETE TASK
+exports.deleteTask = async (req, res) => {
+    const task = await Task.findOneAndDelete({
+        _id: req.params.id,
+        userId: req.user.id
+    });
+
+    if (!task) return res.status(404).json({ message: "Task not found" });
+
+    res.json({ message: "Task deleted" });
+};
+
+// FILTER TASKS
+exports.filterTasks = async (req, res) => {
+    const { category, tag } = req.query;
+
+    let filter = { userId: req.user.id };
+
+    if (category) filter.category = category;
+    if (tag) filter.tags = tag;
+
+    const tasks = await Task.find(filter);
+
+    res.json(tasks);
 };
